be8b631a7308c389aea578c751af74d2d64a9d83
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-06-17 17:01:42 -0500

    Format comments using prettier-plugin-drone-jsdoc

--- src/regex.ts
+++ src/regex.ts
@@ -1,10 +1,8 @@
-/**
- * I'm a regular expression to match capital letters, provided zhey are placed after any letter.
+/**I'm a regular expression to match capital letters, provided they are placed after any letter.
  *
- * will match these cases
- * 	1. lowerUpper
- * 					^
- *	2. UUper
- *			^^
+ * Will match these cases:
+ *
+ * 1. Lower[U]pper
+ * 2. [U][U]per
  */
 export const regularExpression = /(?:(?<=\b[a-zA-Z0-9]*[a-z0-9])[A-Z](?=[a-zA-Z0-9]*\b))|(?:(?<=\b[a-zA-Z0-9]*[A-Z])[A-Z][a-z](?=[a-zA-Z0-9]*\b))/g
